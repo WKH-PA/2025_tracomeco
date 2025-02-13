fa97f28a2e1a93262b86347ebaed5c9d377baa56
Minh bÉO đây =)) <112363274+minhgiang2002@users.noreply.github.com>
2025-02-13 10:49:58 +0700

    1436469-upload 13/02

--- sources/mang_xa_hoi.php
+++ sources/mang_xa_hoi.php
@@ -1,15 +1,30 @@
 <?php
-  $mangxahoi = SHOW_mxh();
-  foreach ($mangxahoi as $rows) {
-  	if($thongtin['mxh_is_anh'] == 1){
+$mangxahoi = SHOW_mxh();
+foreach ($mangxahoi as $rows) {
+    // Kiểm tra nếu có hình ảnh
+    $icon_url = checkImage($fullpath, $rows['icon'], $rows['duongdantin']);
+    if ($thongtin['mxh_is_anh'] == 1) {
+        if(!empty($rows['icon'])){
+        ?>
+        <a title="<?=$rows['tenbaiviet_'.$lang] ?>" href="<?= $rows['duongdantin']?>" target="_blank" rel="nofollow noopener" class="button"
+           style="<?=$rows['background'] ? 'background: '.$rows['background'] : '' ?>">
+            <img src="<?=$icon_url ?>" alt="<?=$rows['tenbaiviet_'.$lang] ?> " height="100%" style="position:absolute; top:50%; left:50%; transform: translate(-50%,-50%);">
+        </a>
+        <?php
+        } else { // Nếu không có hình, hiển thị icon
+            ?>
+            <a title="<?=$rows['tenbaiviet_'.$lang] ?>" href="<?= $rows['duongdantin']?>" target="_blank" rel="nofollow noopener"
+               style="<?=$rows['background'] ? 'background: '.$rows['background'] : '' ?>">
+                <i class="<?=$rows['fontawesome'] ?>"></i>
+            </a>
+            <?php
+        }
+    }else{
 ?>
-        <a title="<?=$rows['tenbaiviet_'.$lang] ?>" href="<?= $rows['duongdantin']?>" target="_blank" rel="nofollow noopener" class="button <?=$rows['fontawesome'] ?> "
-           style="<?=$rows['background'] != '' ?  'background: '.$rows['background'] : '' ?>">
-  <span><?=$rows['tenbaiviet_'.$lang] ?></span>
-  <img src="<?=checkImage($fullpath, $rows['icon'], $rows['duongdantin']) ?>" alt="">
+<a title="<?=$rows['tenbaiviet_'.$lang] ?>" href="<?= $rows['duongdantin']?>" target="_blank" rel="nofollow noopener"
+   style="<?=$rows['background'] ? 'background: '.$rows['background'] : '' ?>">
+    <i class="<?=$rows['fontawesome'] ?>"></i>
 </a>
-<?php }else{ ?>
-        <a title="<?=$rows['tenbaiviet_'.$lang] ?>" href="<?= $rows['duongdantin']?>" target="_blank" rel="nofollow noopener"
-           style="<?=$rows['background'] != '' ?  'background: '.$rows['background'] : '' ?>"><i class="<?=$rows['fontawesome'] ?>"></i></a>
-<?php }} ?>
-
+<?php
+        }
+}?>
