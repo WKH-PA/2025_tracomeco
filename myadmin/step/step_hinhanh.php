d5918d33a107f804dd810c0f761eec33c275417a
Hieu Trinh <tthieu002@gmail.com>
2025-02-14 16:37:32 +0700

    Khắc phục sự cố box lĩnh vực hoạt động ở trang home

--- myadmin/step/step_hinhanh.php
+++ myadmin/step/step_hinhanh.php
@@ -83,9 +83,8 @@
     </div>
 <?php } ?>
 <?php //if (in_array($step, $array_only_bv)) { ?>
-<?php if ($step == 1||$step == 5 || $step == 9 || $step == 10 || $step == 11|| $step == 12) { ?>
+
     <div class="form-group">
         <label>Thuộc chủ đề</label>
         <?= LAY_chude($id_parent, $step, 'id_parent', 'form-control SlectBox', 0, $id_step, 0, 'true', 1) ?>
     </div>
-<?php } ?>
